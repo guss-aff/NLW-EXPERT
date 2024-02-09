7d8c50fba7fb8f0b73cf14f54240975104804f90
Gustavo Amancio <gustavoamaf7@gmail.com>
2024-02-09 11:46:08 -0300

    Implementing error handler

--- src/main/routes/tag_routes.py
+++ src/main/routes/tag_routes.py
@@ -1,15 +1,19 @@
 from flask import Blueprint,request,jsonify
 from src.views.http_types.htttp_request import HttpRequest
 from src.views.tag_creator_vw import TagCreatorView
-
+from src.errors.error_handler import handle_errors
 
 tags_routes_bp = Blueprint('tags_routes',__name__)
 
 @tags_routes_bp.route('/create-tag', methods=['POST'])
 def create_tags():
-  tag_creator_view = TagCreatorView()
-  
-  http_request = HttpRequest(body=request.json)
-  response = tag_creator_view.validate_and_create(http_request)
-  print(response)
+  response = None
+  try:
+    tag_creator_view = TagCreatorView()
+    
+    http_request = HttpRequest(body=request.json)
+    response = tag_creator_view.validate_and_create(http_request)
+    print(response)
+  except Exception as e:
+    response = handle_errors(e)
   return jsonify(response.body),response.status_code
--- src/views/tag_creator_vw.py
+++ src/views/tag_creator_vw.py
@@ -1,17 +1,18 @@
 from src.views.http_types.htttp_request import HttpRequest
 from src.views.http_types.http_response import HttpResponse
-
+from src.controllers.tag_creator_ctrl import TagCratorController
 class TagCreatorView:
   """Responsible for http interactions
   """
   
   def validate_and_create(self,http_request:HttpRequest) -> HttpResponse:
+    tag_creator_controller = TagCratorController()
     
     body = http_request.body
     product_code = body['product_code']
     
     
     #! lógica de negócio
-    
+    formatted_response = tag_creator_controller.create(product_code)
     #! retorno http
-    return HttpResponse(status_code=200,body={"hello":"world"})
+    return HttpResponse(status_code=200,body=formatted_response)
