cd371c8d3a9185f5c22b1cd87764fba4fe5944ab
Gustavo Amancio <gustavoamaf7@gmail.com>
2024-02-09 13:46:42 -0300

    Implementinh Unprocessable Error

--- src/errors/error_handler.py
+++ src/errors/error_handler.py
@@ -1,7 +1,17 @@
 from src.views.http_types.http_response import HttpResponse
-
+from .errors_types.http_unprocessable_entity import HttpUnprocessableEntityError
 
 def handle_errors(error: Exception) -> HttpResponse:
+  if isinstance(error,HttpUnprocessableEntityError):
+    return HttpResponse(
+      status_code=error.status_code,
+      body={
+        "errors":[{
+          "title": error.name,
+          "detail": error.message
+        }]
+      }
+    )
   return HttpResponse(
     status_code=500,
     body={
--- src/main/routes/tag_routes.py
+++ src/main/routes/tag_routes.py
@@ -2,6 +2,7 @@ from flask import Blueprint,request,jsonify
 from src.views.http_types.htttp_request import HttpRequest
 from src.views.tag_creator_vw import TagCreatorView
 from src.errors.error_handler import handle_errors
+from src.validators.tag_creator_validator import tag_creator_validator
 
 tags_routes_bp = Blueprint('tags_routes',__name__)
 
@@ -9,9 +10,11 @@ tags_routes_bp = Blueprint('tags_routes',__name__)
 def create_tags():
   response = None
   try:
+    tag_creator_validator(request)
     tag_creator_view = TagCreatorView()
     
     http_request = HttpRequest(body=request.json)
+    
     response = tag_creator_view.validate_and_create(http_request)
     print(response)
   except Exception as e:
